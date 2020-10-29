a5baef94efc68d926b1a2794227457dae00db0e2
ninahyeon <ninahyeon@gmail.com>
2020-10-29 15:12:28 +0900

    모바일용 헤더 OK

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* 함수의 기본 작동원리
+	function aname(vars) {	//함수선언
+		alert(vars + " 이름출력");
+	}
+	aname('김나현');	//함수호출
+	*/
+	$(document).ready(function(){
+		//alert('콜백함수 실행');
+		$(".openMOgnb").click(function(){
+			//alrert()//디버그용
+			// $(".header_cont").css("display", "block");
+			$(".header_cont").slideDown("slow");
+		//header 아이디영역 백그라운드 추가
+		$("#header").addClass("on");
+		});
+		$(".closePop").click(function(){
+			// $(".header_cont").css("display", "none");
+			$(".header_cont").slideUp("fast");
+			$("#header").removeClass("on");
+		});
+	});
